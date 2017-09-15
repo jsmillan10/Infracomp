26ca79b9472f201cd7cc8e3ccabdcef3a1dace5f
NICOLAS AGUILAR LEON <n.aguilar@sis.virtual.uniandes.edu.co>
2017-09-15 11:30:31 -0500

    Cliente

--- src/manejo_concurrencia/Cliente.java
+++ src/manejo_concurrencia/Cliente.java
@@ -1,8 +1,19 @@
 package manejo_concurrencia;
 
 public class Cliente {
+	
+	private Mensaje[] mensajes;
+
+	public Cliente(int numMen)
+	{
+		mensajes = new Mensaje [numMen];
+		for (int i = 0; i < mensajes.length; i++) {
+			mensajes[i]= new Mensaje (i);
+		}
+	}
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 
 	}
