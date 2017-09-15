7911a3e98b41ce2e69a0c7537a51597780e76b9b
Sebastian Millan <js.millan10@uniandes.edu.co>
2017-09-15 11:32:27 -0500

    Hola

--- src/manejo_concurrencia/Cliente.java
+++ src/manejo_concurrencia/Cliente.java
@@ -1,7 +1,9 @@
 package manejo_concurrencia;
 
-public class Cliente {
+public class Cliente extends Thread{
 
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
