f2c65b6f6c73f18890144a5ff95533a5ce363123
naguilar12 <n.aguilar@uniandes.edu.co>
2017-09-16 17:08:58 -0500

    Leer numero de mensajes por cliente

--- src/manejo_concurrencia/Cliente.java
+++ src/manejo_concurrencia/Cliente.java
@@ -1,9 +1,12 @@
 package manejo_concurrencia;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
 
 public class Cliente extends Thread{
 
@@ -42,11 +45,19 @@ public class Cliente extends Thread{
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		BufferedReader lector = new BufferedReader(new FileReader("./datos/datos.txt"));
-		int numClientes = Integer.parseInt(lector.readLine());
-		int numServidores = Integer.parseInt(lector.readLine());;
-		int capacidadBuffer = Integer.parseInt(lector.readLine());;
-
+		Properties pro = new Properties();
+		
+		pro.load(new FileInputStream(new File("datos/datos.properties")));
+		int numClientes = Integer.parseInt(pro.getProperty("numClientes"));
+		int numServidores = Integer.parseInt(pro.getProperty("numServidores"));
+		int capacidadBuffer = Integer.parseInt(pro.getProperty("capacidadBuffer"));
+		
+		int[] mensajesPorCliente = new int[numClientes];
+		
+		for (int i = 0; i < mensajesPorCliente.length; i++)  {
+			mensajesPorCliente[i] = Integer.parseInt(pro.getProperty("cliente" + (i+1)));
+		}
+		
 		Buffer buffer = new Buffer(capacidadBuffer,numClientes);
 		Mensaje.buffer = buffer;
 		Servidor.buffer = buffer;
@@ -56,7 +67,7 @@ public class Cliente extends Thread{
 		}
 
 		for (int i = 0; i < numClientes; i++) {
-			(new Cliente(10)).start();
+			(new Cliente(mensajesPorCliente[i])).start();
 		}
 	}
 
